bc602c66b16b87934cf7552034242a2a40f5c443
ck-unext <s-chuenkai@unext.jp>
2022-07-19 18:39:57 +0900

    diversity & serendipity

--- ds_metrics.py
+++ ds_metrics.py
@@ -1,6 +1,6 @@
 import os, logging
 import rich_click as click
-from non_acc_metrics import CatalogCoverage, Novelty
+from non_acc_metrics import CatalogCoverage, Novelty, Diversity, Serendipity
 #from dstools.connectors.bigquery import Uploader
 from tobq import BigQuery
 logging.basicConfig(level=logging.INFO)
@@ -23,6 +23,34 @@ def novelty(reco_path, training_data_path, black_list_path):
     n = Novelty(training_data_path, black_list_path)
     n.calculate(reco_path)
 
+@click.command()
+@click.option('--black_list_path', help='csv of black list SIDs')
+@click.argument('reco_path')
+@click.argument('training_data_path')
+def novelty(reco_path, training_data_path, black_list_path):
+    click.echo(f"novelty : {reco_path} {training_data_path} {black_list_path} ")
+    n = Novelty(training_data_path, black_list_path)
+    n.calculate(reco_path)
+
+@click.command()
+@click.argument('reco_path')
+@click.argument('model_path')
+@click.argument('sakuhin_meta_path')
+def diversity(model_path, reco_path, sakuhin_meta_path):
+    click.echo(f"diversity : {reco_path} {model_path} {sakuhin_meta_path} ")
+    metric = Diversity(model_path, sakuhin_meta_path)
+    metric.calculate(reco_path)
+
+@click.command()
+@click.argument('reco_path')
+@click.argument('model_path')
+@click.argument('sakuhin_meta_path')
+@click.argument('history_path')
+def serendipity(model_path, reco_path, sakuhin_meta_path, history_path):
+    click.echo(f"serendipity : {reco_path} {model_path} {sakuhin_meta_path} {history_path}")
+    metric = Serendipity(model_path, sakuhin_meta_path, history_path)
+    metric.calculate(reco_path)
+
 
 @click.command()
 @click.argument('csv_path')
@@ -43,6 +71,8 @@ def cli():
 
 cli.add_command(catalog_coverage)
 cli.add_command(novelty)
+cli.add_command(diversity)
+cli.add_command(serendipity)
 cli.add_command(tobq)
 
 if __name__ == '__main__':
--- non_acc_metrics.py
+++ non_acc_metrics.py
@@ -4,19 +4,20 @@ import csv
 import datetime
 from collections import Counter
 import numpy as np
-import pickle
-# from dscollaborative.recommender import ImplicitModel
+import sklearn.metrics.pairwise as pairwise
+from dscollaborative.recommender import ImplicitModel
+from scipy.sparse import lil_matrix
+from numpy import dot
+from numpy.linalg import norm
+import math
 from autoalts.utils import efficient_reading
 
 logging.basicConfig(level=logging.INFO)
 
 
-def load_model(path):
-    return pickle.load(open(path, "rb"))
-
-
 class NonAccMetrics:
     def __init__(self, metric_name, black_list_path: str = None):
+        self.miniters = 500000  # for tqdm, update progress per miniters
         self.metric_name = metric_name
         self.all_value = 0
         self.autoalt_value = 0
@@ -27,9 +28,16 @@ class NonAccMetrics:
 
     @staticmethod
     def load_model(model_path):
-        # model = ImplicitModel()
-        model = load_model(model_path)
+        model = ImplicitModel()
+        model.load_model(model_path)
         return model
+
+    @staticmethod
+    def cos_similarity(a, b):
+        if any(a) and any(b):
+            return dot(a, b)/(norm(a)*norm(b))
+        else:  # if one of input is all zeros -> one of input has no features -> skip
+            return np.nan
 
     def load_black_list(self, black_list_path):
         for line in efficient_reading(black_list_path, with_header=True):
@@ -48,6 +56,165 @@ class NonAccMetrics:
             writer.writerow(basic_params)
 
 
+class SimilarityMetric(NonAccMetrics):
+    def __init__(self, metric_name, model_path, sakuhin_meta_path: str):
+        super(SimilarityMetric, self).__init__(metric_name)
+        self.feat_idx_lookup = self.make_item_feat_vectors(sakuhin_meta_path)
+        # hard-coded for v1
+        self.feat_idx_table = {'JFET000002':0, 'JFET000003':1, 'JFET000004':2, 'JFET000005':3,
+                               'JFET0000061':4, 'JFET0000062':4, 'JFET0000063':4, 'JFET0000064':4, 'JFET0000065':4, 'JFET0000066':4, 'JFET0000067':4,
+                               'JFET000007':5, 'JFET000008':6, 'chotatsu':7}
+        self.feat_names = ['byw', 'new arrival EPs', 'popular', 'trending', 'tag', 'exclusives', 'new_arrival SIDs', 'chotatsu']
+        model = self.load_model(model_path)
+        self.item2id = model.model.user_item_matrix.item2id
+        self.user2id = model.model.user_item_matrix.user2id
+        del model
+
+        self.item_feat_matrix = np.zeros((len(self.item2id), len(self.feat_idx_lookup)))
+        genre_weight = 2
+        with open(sakuhin_meta_path, "r") as csv_path:
+            reader = csv.DictReader(csv_path)
+            for line in tqdm(reader, miniters=self.miniters):
+                item_idx = self.item2id.get(line['sakuhin_public_code'], None)
+                if not item_idx:
+                    continue
+
+                genre_idx = self.feat_idx_lookup.get(line['main_genre_code'], None)
+                if not genre_idx:
+                    continue
+                self.item_feat_matrix[item_idx, genre_idx] = genre_weight
+
+                tag_pool = set()
+                tag_pool.update(line['menu_names'].split('|'))
+                tag_pool.update(line['parent_menu_names'].split('|'))
+                for tag in tag_pool:
+                    tag_idx = self.feat_idx_lookup.get(tag, None)
+                    self.item_feat_matrix[item_idx, tag_idx] = 1
+                # nation_pool.update(line['nations'].split('/'))
+        logging.info(f"item_feat_matrix ({self.item_feat_matrix.shape}) built")
+
+    @staticmethod
+    def pairwise_matrix_cos_sim(vectors):
+        s = pairwise.cosine_similarity(vectors)
+        div = (2*(s.shape[0]*s.shape[0]-s.shape[0])/2)
+        if div <= 0.00001:
+            return 0
+        else:
+            return (np.sum(s) - s.shape[0])/div
+
+    @staticmethod
+    def make_item_feat_vectors(sakuhin_meta_path: str) -> dict:
+        genre_pool = set()  # main_genre_code
+        tag_pool = set()  # menu_names
+        #nation_pool = set()
+        with open(sakuhin_meta_path, "r") as csv_path:
+            reader = csv.DictReader(csv_path)
+            for line in reader:
+                genre_pool.add(line['main_genre_code'])
+                tag_pool.update(line['menu_names'].split('|'))
+                tag_pool.update(line['parent_menu_names'].split('|'))
+
+        feat_idx_lookup = dict()
+        for i, x in enumerate((genre_pool | tag_pool) - {''}) :
+            feat_idx_lookup[x] = i
+        return feat_idx_lookup
+
+
+class Serendipity(SimilarityMetric):
+    def __init__(self, model_path: str, sakuhin_meta_path: str, user_sid_history_path: str):
+        super(Serendipity, self).__init__('serendipity', model_path, sakuhin_meta_path)
+        self.top_n = 5  # only run on top N items
+
+        # build history record matrix
+        logging.info("")
+        self.history_matrix = lil_matrix((len(self.user2id), len(self.item2id)), dtype=int)
+        # history SIDs may be too heavy
+        # user_multi_account_id,sids,genres,play_times
+        with open(user_sid_history_path, "r") as csv_path:
+            reader = csv.DictReader(csv_path)
+            for line in tqdm(reader, miniters=self.miniters):
+                user_idx = self.user2id.get(line['user_multi_account_id'], None)
+                sids = line["sids"].split("|")[:self.top_n]
+                sid_indices = [self.item2id[x] for x in sids if x in self.item2id]
+                if not user_idx or not sid_indices:
+                    continue
+                self.history_matrix[user_idx, sid_indices] = 1
+
+    def cal_unserendipity(self, history, reco):
+        overall_sim_list = []
+        for h_i in history:
+            for r_i in reco:
+                cos_sim = self.cos_similarity(self.item_feat_matrix[h_i], self.item_feat_matrix[r_i])
+                if not math.isnan(cos_sim):
+                    overall_sim_list.append(cos_sim)
+
+        return np.mean(overall_sim_list)
+
+    def calculate(self, reco):
+        metric_matrix = np.zeros((len(self.user2id), self.feat_idx_table['chotatsu']+1))
+
+        with open(reco, "r") as csv_path:
+            reader = csv.DictReader(csv_path)
+            for line in tqdm(reader, miniters=self.miniters):
+                if line["user_multi_account_id"] not in ["non-logged-in-coldstart", "coldstart"]:
+                    user_idx = self.user2id.get(line['user_multi_account_id'], None)
+                    sids = line["sakuhin_codes"].split("|")[:self.top_n]
+                    r_sid_indices = [self.item2id[x] for x in sids if x in self.item2id]
+                    if not user_idx or not r_sid_indices:
+                        continue
+                    # history
+                    h_sid_indices = self.history_matrix.rows[user_idx]
+                    if not h_sid_indices:
+                        continue
+
+                    alt_idx = self.feat_idx_table.get(line['feature_public_code'], self.feat_idx_table['chotatsu'])
+                    serendipity_value = self.cal_unserendipity(h_sid_indices, r_sid_indices)
+                    # print(f"serendipity_value = {serendipity_value}")
+                    if not math.isnan(serendipity_value):
+                        metric_matrix[user_idx, alt_idx] = serendipity_value
+
+        alt_sim_sums = np.sum(metric_matrix, axis=0)
+        alt_sim_cnts = np.count_nonzero(metric_matrix, axis=0)
+        for name, alt_sim_sum, alt_sim_cnt in zip(self.feat_names, alt_sim_sums, alt_sim_cnts):
+            logging.info("{} = {:.2f}%".format(name, (1.0 - (alt_sim_sum/alt_sim_cnt))*100))
+
+        self.all_value = 1 - np.sum(alt_sim_sums)/np.sum(alt_sim_cnts)
+        self.autoalt_value = 1 - np.sum(alt_sim_sums)[:-1]/np.sum(alt_sim_cnts)[:-1]
+        self.chotatsu_value = 1 - np.sum(alt_sim_sums)[-1]/np.sum(alt_sim_cnts)[-1]
+        self.output()
+
+
+class Diversity(SimilarityMetric):
+    def __init__(self, model_path: str, sakuhin_meta_path: str):
+        super(Diversity, self).__init__('diversity', model_path, sakuhin_meta_path)
+
+    def calculate(self, reco):
+        metric_matrix = np.zeros((len(self.user2id), self.feat_idx_table['chotatsu']+1))
+        with open(reco, "r") as csv_path:
+            reader = csv.DictReader(csv_path)
+            for line in tqdm(reader, miniters=self.miniters):
+                if line["user_multi_account_id"] not in ["non-logged-in-coldstart", "coldstart"]:
+                    sids = line["sakuhin_codes"].split("|")
+                    user_idx = self.user2id.get(line['user_multi_account_id'], None)
+                    sid_indices = [self.item2id[x] for x in sids if x in self.item2id]
+                    if not user_idx or not sid_indices:
+                        continue
+                    alt_idx = self.feat_idx_table.get(line['feature_public_code'], self.feat_idx_table['chotatsu'])
+                    similarity = self.pairwise_matrix_cos_sim(self.item_feat_matrix[sid_indices])
+                    if similarity:
+                        metric_matrix[user_idx, alt_idx] = similarity
+
+        alt_sim_sum = np.sum(metric_matrix, axis=0)
+        alt_sim_cnt = np.count_nonzero(metric_matrix, axis=0)
+        for name, v in zip(self.feat_names, alt_sim_sum/alt_sim_cnt):
+            logging.info("{} = {:.2f}%".format(name, (1-v)*100))
+
+        self.all_value = 1 - np.sum(alt_sim_sum)/np.sum(alt_sim_cnt)
+        self.autoalt_value = 1 - np.sum(alt_sim_sum)[:-1]/np.sum(alt_sim_cnt)[:-1]
+        self.chotatsu_value = 1 - np.sum(alt_sim_sum)[-1]/np.sum(alt_sim_cnt)[-1]
+        self.output()
+
+
 class Novelty(NonAccMetrics):
     def __init__(self, training_data, black_list_path):
         super(Novelty, self).__init__('novelty', black_list_path)
@@ -61,7 +228,7 @@ class Novelty(NonAccMetrics):
         logging.info("load_training_data")
         with open(training_data, "r") as csv_path:
             reader = csv.DictReader(csv_path)
-            for line in tqdm(reader):
+            for line in tqdm(reader, miniters=self.miniters):
                 self.users_set.add(line['userid'])
                 self.popularity_cnt[line['item']] += 1
         logging.info(f'popularity_cnt = {len(self.popularity_cnt)}')
@@ -86,7 +253,7 @@ class Novelty(NonAccMetrics):
         chotatsu_alt_cnt = 0
         with open(reco, "r") as csv_path:
             reader = csv.DictReader(csv_path)
-            for line in tqdm(reader):
+            for line in tqdm(reader, miniters=self.miniters):
                 if line["user_multi_account_id"] not in ["non-logged-in-coldstart", "coldstart"]:
                     alt_novelty_value = self.novelty_calculation(line["sakuhin_codes"].split("|"), self.popularity_cnt, self.nb_user)
 
@@ -112,10 +279,10 @@ class CatalogCoverage(NonAccMetrics):
     def __init__(self, model_path, black_list_path=None):
         super(CatalogCoverage, self).__init__('catalog_coverage', black_list_path)
         model = self.load_model(model_path)
-        self.nb_all_users = len(model.user_item_matrix.user2id)
+        self.nb_all_users = len(model.model.user_item_matrix.user2id)
         logging.info(f"nb of all users = {self.nb_all_users}")
         # get all items from mode
-        self.all_sids = set(model.user_item_matrix.item2id.keys())
+        self.all_sids = set(model.model.user_item_matrix.item2id.keys())
         if self.black_sids:
             self.all_sids = self.all_sids - self.black_sids
         logging.info(f"nb of all items = {len(self.all_sids)}")
@@ -127,7 +294,7 @@ class CatalogCoverage(NonAccMetrics):
         # "/Users/s-chuenkai/PycharmProjects/check/autoalt_ippan_features_2022-07-01.csv"
         with open(reco_path, "r") as csv_path:
             reader = csv.DictReader(csv_path)
-            for line in tqdm(reader):
+            for line in tqdm(reader, miniters=self.miniters):
                 if line["user_multi_account_id"] not in ["non-logged-in-coldstart", "coldstart"]:
                     sids = line["sakuhin_codes"].split("|")
                     # reco_sids.update(sids)
